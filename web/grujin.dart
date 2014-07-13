97405726542362bb0534fd4ebe6490fcdbebe23a
Jacob Phillips <jphilli85@gmail.com>
2014-07-13 16:11:56 -0400

    ui basically done

--- web/grujin.dart
+++ web/grujin.dart
@@ -3,15 +3,10 @@ import 'package:polymer/polymer.dart';
 import 'package:paper_elements/paper_input.dart';
 import 'package:paper_elements/paper_toast.dart';
 import 'package:paper_elements/paper_checkbox.dart';
+import 'package:paper_elements/paper_button.dart';
 import 'package:paper_elements/paper_fab.dart';
 
 export 'package:polymer/init.dart';
-
-const int _INDEX_LETTERS  = -1; //unused
-const int _INDEX_UPPER   = 0;
-const int _INDEX_LOWER   = 1;
-const int _INDEX_NUMBERS = 2;
-const int _INDEX_SYMBOLS = 3;
 
 final PaperInput    _subject    = querySelector('#subject');
 final HtmlElement   _secret     = querySelector('#secret');       // must be HtmlElement for .shadowRoot
@@ -25,11 +20,27 @@ final PaperCheckbox _rempassoptions = querySelector('#rempassoptions');
 final DivElement    _leftpanel = querySelector('#leftpanel');
 final PaperToast    _toast      = querySelector('#toast');       
 final HtmlElement   _mainbutton = querySelector('paper-fab');     // must be HtmlElement for selector tag (?) 
-
+final HtmlElement   _captoggle   = querySelector('#captoggle');
+final HtmlElement   _lowtoggle   = querySelector('#lowtoggle');
+final HtmlElement   _numtoggle   = querySelector('#numtoggle');
+final HtmlElement   _symtoggle   = querySelector('#symtoggle');  
+  
 void main() {                
   // DOM is fully loaded. 
   InputElement input = _secret.shadowRoot.olderShadowRoot.children.firstWhere((el) => el.id == 'input');
   input.type = 'password';
- 
+  
+  _captoggle.onClick.listen((e) => _toggleActive(_captoggle));
+  _lowtoggle.onClick.listen((e) => _toggleActive(_lowtoggle));
+  _numtoggle.onClick.listen((e) => _toggleActive(_numtoggle));
+  _symtoggle.onClick.listen((e) => _toggleActive(_symtoggle));
 }  
+
+void _toggleActive(Element e) {
+  if (e.attributes.containsKey('inactive')) {
+    e.attributes.remove('inactive');  
+  } else {
+    e.attributes['inactive'] = 'true';
+  }
+}
   
